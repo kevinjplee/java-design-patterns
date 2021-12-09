2ca54db0e07a5a498e2396be2244a721b9ded29e
kevinjplee197@gmail.com <kevinjplee197@gmail.com>
2021-12-08 21:32:15 -0600

    #1283 comments added

--- lock-manager/src/test/java/com/iluwater/lockmanager/AppTest.java
+++ lock-manager/src/test/java/com/iluwater/lockmanager/AppTest.java
@@ -19,6 +19,8 @@
 *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 *THE SOFTWARE.
+*
+* CS427 Issue link: https://github.com/iluwatar/java-design-patterns/issues/1283
 */
 
 package com.iluwater.lockmanager;
